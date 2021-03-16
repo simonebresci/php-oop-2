24fbc33b38e07ef392bfca4c430bc4f91c35e6b4
Simone Bresci <simone.bresci.05@gmail.com>
2021-03-16 17:57:45 +0100

    classe article

--- index.php
+++ index.php
@@ -111,21 +111,57 @@ class Cart{
   }
 }
 // /CART & SONS =================================================================
-// class article{
-//   private id;
-//   protected name;
-//   protected pathImage;
-//   protected size;
-//   protected prize;
-//
-//   public function __construct(string $id,string $name,string $pathImage,string $size, string $prize) {
-//         $this->id = $id;
-//         $this->name = $name;
-//         $this->pathImage = $pathImage;
-//         $this->size = $size;
-//         $this->prize = $prize;
-//   }
-// }
+class article{
+  private $id;
+  protected $name;
+  protected $pathImage;
+  protected $size;
+  protected $prize;
+
+  public function __construct(string $id,string $name,string $pathImage,string $size, string $prize) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->pathImage = $pathImage;
+        $this->size = $size;
+        $this->prize = $prize;
+  }
+
+  public function getId(){
+    return $this->id;
+  }
+
+  public function getName(){
+    return $this->name;
+  }
+
+  public function getPathImage(){
+    return $this->pathImage;
+  }
+  public function getsize(){
+    return $this->size;
+  }
+
+  public function getPrize(){
+    return $this->prize;
+  }
+
+  public function setName($name){
+    $this->password = $name;
+  }
+
+  public function setPathImage($pathImage){
+    $this->password = $pathImage;
+  }
+
+  public function setSize($size){
+    $this->password = $size;
+  }
+
+  public function setPrize($prize){
+    $this->password = $prize;
+  }
+
+}
 
 
 
@@ -141,6 +177,11 @@ $pippoSigned->setPassword("pipponazionale");
 
 $cartPippo = new Cart("cart_001","14785","VISA","2025/07/01");
 $pippoSigned->setCart($cartPippo);
+
+
+$article1 = new Article("art_001","Sedia","img/art_001","33x33x33cm","9,99€");
+$article2 = new Article("art_002","Tavolo","img/art_002","33x33x33cm","9,99€");
+$article3 = new Article("art_003","Televisore","img/art_003","33x33x33cm","9,99€");
 
 ?>
 
@@ -163,6 +204,30 @@ $pippoSigned->setCart($cartPippo);
       <?php echo "Name:" . $cartPippo->getPin() . "<br>" ?>
       <?php echo "Surname:" . $cartPippo->getBrand() . "<br>" ?>
       <?php echo "Expiration:" . $cartPippo->getExpiration() . "<br>" ?>
+    </div>
+    <hr>
+    <div class="articles">
+
+      <h3> Articolo 1 </h3>
+      <?php echo "ID:" . $article1->getId() . "<br>"?>
+      <?php echo "Name:" . $article1->getName() . "<br>" ?>
+      <?php echo "Surname:" . $article1->getPathImage() . "<br>" ?>
+      <?php echo "Expiration:" . $article1->getSize() . "<br>" ?>
+      <?php echo "Expiration:" . $article1->getPrize() . "<br>" ?>
+
+      <h3> Articolo 2 </h3>
+      <?php echo "ID:" . $article2->getId() . "<br>"?>
+      <?php echo "Name:" . $article2->getName() . "<br>" ?>
+      <?php echo "Surname:" . $article2->getPathImage() . "<br>" ?>
+      <?php echo "Expiration:" . $article2->getSize() . "<br>" ?>
+      <?php echo "Expiration:" . $article2->getPrize() . "<br>" ?>
+
+      <h3> Articolo 3 </h3>
+      <?php echo "ID:" . $article3->getId() . "<br>"?>
+      <?php echo "Name:" . $article3->getName() . "<br>" ?>
+      <?php echo "Surname:" . $article3->getPathImage() . "<br>" ?>
+      <?php echo "Expiration:" . $article3->getSize() . "<br>" ?>
+      <?php echo "Expiration:" . $article3->getPrize() . "<br>" ?>
     </div>
 
   </body>
